485067d6fc7755420908f6d2845d0a08c1a70de9
Travis-Owens <30815131+Travis-Owens@users.noreply.github.com>
2019-08-12 14:06:43 -0400

    Migrate service fetch to database.py
    Migrated fetching services to modules/database.py
    
    def get_services()
    
    Additionally, removed unused variables.

--- modules/database.py
+++ modules/database.py
@@ -6,3 +6,15 @@ import pymysql
 class database(object):
     def __init__(self):
         pass
+
+    def get_services(self, queue):
+        # TODO: MySQL integration
+
+
+        # TODO: Implement DB fetch here
+        services = [
+                {'service_id':1, 'service_name': 'google.com', 'service_type':'http', 'url':'http://google.com', 'last_checked_status': True, 'notification_email' : True, 'notification_sms': True, 'email': 'g@t.com', 'phone_number': '44444'},
+        ]
+
+        for service in services:
+            queue.put(service)
