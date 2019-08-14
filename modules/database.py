b1231abf8e59cd9fc87ee61ba89fc462c69fcf0d
Mathis Van Eetvelde <mathis.vaneetvelde@gmail.com>
2019-08-14 21:05:05 +0200

    Added exception logging
    have yet to make the file log.

--- modules/database.py
+++ modules/database.py
@@ -22,13 +22,29 @@ class database(object):
 			return connection
 
 		except Exception as e:
+			print("COULD NOT ESTABLISH CONNECTION TO DATABASE. PLEASE CHECK config.py !STOPPING ...")
 			print(e)
-			print("Can't fetch service database. Stopping ...")
 			exit()
 
-		
+	def create_exceptions_table(self, connection):
+		with connection.cursor() as cursor:
+			try:
+				cursor.execute("CREATE TABLE exceptions (log_id int NOT NULL AUTO_INCREMENT PRIMARY KEY, exception TEXT, timestamp TIMESTAMP DEFAULT now() ON UPDATE now())")
+				connection.commit()
+				print("Table 'exceptions' succesfully created!")
 
-	def create_table(self, connection):
+			except pymysql.MySQLError as e:
+				if("1050" in str(e)): #1050 is warning code for table allready exists
+					print("Found 'exceptions' table!")
+				else:
+					print("COULD NOT ESTABLISH CONNECTION TO 'EXCEPTIONS' DATABSE! REASON:")
+					print(e)
+
+			except Exception as e:
+				print("COULD NOT ESTABLISH CONNECTION TO 'EXCEPTIONS' DATABSE! REASON:")
+				print(e)	
+
+	def create_services_table(self, connection):
 		with connection.cursor() as cursor:
 			try:
 				cursor.execute("CREATE TABLE services (service_id int NOT NULL AUTO_INCREMENT PRIMARY KEY, service_created TIMESTAMP DEFAULT now(), last_updated TIMESTAMP DEFAULT now() ON UPDATE now(), service_name TEXT, service_type TEXT, service_address TEXT, last_checked_status BOOLEAN, notification_email BOOLEAN, notification_sms BOOLEAN, email TEXT, phone_number TEXT)")
@@ -54,24 +70,42 @@ class database(object):
 
 			if(len(rows) > 0):
 				for service in rows:
-					print("Found service: {}".format(service["service_address"]))
 					self.services.append(service)
+
+				print("Found {} services!".format(len(self.services)))
+
 			else:
 				print("No services in database! Stopping ...")
 				exit()
 
-				
+	def log_error(self, error_message):
+		temp_connection = self.connection
+
+		with temp_connection.cursor() as cursor:
+			try:
+				cursor.execute("""INSERT INTO exceptions (exception) VALUES ("%s")""" % (error_message))
+				temp_connection.commit()
+				print("succesfully logged exception in database")
+			except Exception as e:
+				print(e)
+				print("!! Could not log exception!!")
+				#have to write exception log file thing in here		
+
+
 	def get_services(self, queue, db_config):
 
 		print("Attempting to connect to: '{}' at: '{}' as: '{}'".format(db_config["DB_DATABASE"], db_config["DB_HOST"], db_config['DB_USER']))
-		connection = self.create_connection(db_config)
-		print("Connected!")
+		self.connection = self.create_connection(db_config)
+		print("Connected to database!")
 
 		#Create tables if not exist, we run this every time just to be safe
-		self.create_table(connection)
+		self.create_exceptions_table(self.connection)
+		self.create_services_table(self.connection)
+
+		self.log_error("peepeepoopoo test123")
 
 		#fetches all services form DB and adds it to self.services
-		self.fetch_services(connection)
+		self.fetch_services(self.connection)
 
 		for service in self.services:
 			queue.put(service)
