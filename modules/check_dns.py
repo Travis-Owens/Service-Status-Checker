f89019b75ee1c0d05d7ac25a94297d9b52952de8
Travis-Owens <30815131+Travis-Owens@users.noreply.github.com>
2019-08-16 10:09:30 -0400

    check_dns
    Created a function to check the resolvability of a domain name.
    
    This module uses the socket lib and should be compatible with both windows and linux.
    
    - Normailzes hostname by removing https:// or http:// if it exist.
    - Attempts to resolve domain name
    - Calls the service_status_manager event function

--- modules/check_dns.py
+++ modules/check_dns.py
@@ -1,6 +1,46 @@
 # Author: @Travis-Owens
 # Date:   2019-7-31
 
+# External Import
+import socket
+
+# Internal Imports
+from modules.log import logging
+from modules.service_status_manager import service_status_manager as ssm
+
 class check_dns(object):
-    def __init__(self):
-        pass
+    def __init__(self, service):
+        self.service = service
+
+    def check(self):
+        try:
+            # gethostbyname does not work with the prefix http or https
+            if( self.service['service_address'].startswith('http://') ):
+                hostname = self.service['service_address'][7:]
+            elif( self.service['service_address'].startswith('https://') ):
+                hostname = self.service['service_address'][8:]
+            else:
+                hostname = self.service['service_address']
+
+            # Attempt to resolve hostname
+            query = socket.gethostbyname(hostname)
+
+            # If gethostbyname is unable to resolve the domain to an IP it will
+            # trigger the exception below, if the script reaches this point
+            # the status is True
+            status = True
+
+        except socket.gaierror as e:
+            # unable to reslove hostname to an IP
+            status = False
+
+        except Exception as e:
+            logging().log_error(e)
+
+        # Initialize service_status_manager
+        if(status == True):
+            ssm().event(self.service, True)
+        else:
+            ssm().event(self.service, False)
+
+        return
