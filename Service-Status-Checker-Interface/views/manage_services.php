bb7052bdcd7148c4addc7b1c78801fb0a488d518
Travis-Owens <30815131+Travis-Owens@users.noreply.github.com>
2019-10-07 09:06:43 -0400

    Front-End Interface Init
    Initial PHP for front-end interface.

---
+++ Service-Status-Checker-Interface/views/manage_services.php
@@ -0,0 +1,27 @@
+<?php
+# Author: Travis-Owens
+# Date:   2019-8-17
+# Description: This page generates a table that list the services in the DB
+
+// require_once("config.php");
+
+class manage_services {
+  public function __construct() {
+    print('manage_services');
+  }
+
+  // public function create_table() {
+  //
+  //   $services = $this->fetch_services();
+  //
+  //
+  // }
+  //
+  // public function fetch_services() {
+  //   $db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);      // Define Connection parms. (from config.php)
+  //   $user_name = $_SESSION['user_name'];                                  // Define the user to lookup
+  //   $sql = "SELECT * FROM users";  // Retrive data
+  //   $result = $db_connection->query($sql);                               // Storing retreived data
+  // }
+
+}
